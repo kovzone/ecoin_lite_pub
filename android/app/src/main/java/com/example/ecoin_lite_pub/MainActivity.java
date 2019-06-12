c02a7bffadbe8a75ebeb42772d2d07402a67bf50
Lutfi Aziz Firdaus <lutfi.afirdaus@gmail.com>
2019-06-13 02:24:40 +0700

    first commit with flutter

---
+++ android/app/src/main/java/com/example/ecoin_lite_pub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ecoin_lite_pub;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
